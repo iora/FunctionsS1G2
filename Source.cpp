8839cf9be698be21c18c1853bc461902eeafd1d3
PhyrumPhipop <prithchea@zamanuniversity.edu.kh>
2017-05-05 12:08:03 +0700

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -96,14 +96,6 @@ void printAsChars(int myArray[]) {
 }
 
 //Phyrum Rithchea
-/* Print Group Member names */
-void printGroupMemberNames()
-{
-  printf("Phyrum Rithchea\nLiza Song\nReaksmey Kongkea Chea\nBorey Cheng\n"); // Print member names by starting with a new line
-  return 0;
-}
-
-//Phyrum Rithchea
 /* This code is used to find the average of the array (myArray)*/
 double getAverage(int myArray[]);
 {
